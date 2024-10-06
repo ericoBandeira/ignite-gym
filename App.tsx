0eb6fb08c38f79094d728c8df6ddc001f9e31e4b
Érico Maximiano Bandeira <ericobandeira@hotmail.com>
2024-10-06 17:42:08 -0300

    Add assets and add libraries

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
 import { StatusBar } from "react-native";
 import { Home } from "./src/screens/Home";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import { Loading } from "@components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
+
   return (
     <>
       <StatusBar
@@ -9,7 +17,7 @@ export default function App() {
         translucent
         backgroundColor="transparent"
       />
-      <Home />
+      {fontsLoaded ? <Home /> : <Loading />}
     </>
   );
 }
