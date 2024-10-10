d2291d84830e54580359a2ae3d52443ee7f4b2f9
Érico Maximiano Bandeira <ericobandeira@hotmail.com>
2024-10-09 23:02:48 -0300

    Fix some bugs and add logo

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,8 @@
-import { Image, VStack } from "@gluestack-ui/themed";
+import { Center, Image, Text, VStack } from "@gluestack-ui/themed";
+
 import BackgroundImg from "@assets/background.png";
+import Logo from "@assets/logo.svg";
+
 export function SignIn() {
   return (
     <VStack flex={1} bg="$gray700">
@@ -11,6 +14,14 @@ export function SignIn() {
         alt="Pessoas treinando"
         position="absolute"
       />
+
+      <Center my="$24">
+        <Logo />
+
+        <Text color="$gray100" fontSize="$sm">
+          Treine sua mente e seu corpo
+        </Text>
+      </Center>
     </VStack>
   );
 }
