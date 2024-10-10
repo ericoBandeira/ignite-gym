0608c049248aba141d460c92c35a91b309f52780
Érico Maximiano Bandeira <ericobandeira@hotmail.com>
2024-10-09 21:57:51 -0300

    Create SignIn page

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
-/* eslint-disable camelcase */
 import { StatusBar } from "react-native";
 import {
   useFonts,
   Roboto_700Bold,
   Roboto_400Regular,
 } from "@expo-google-fonts/roboto";
-import { Center, GluestackUIProvider, Text } from "@gluestack-ui/themed";
+import { GluestackUIProvider } from "@gluestack-ui/themed";
 import { config } from "./config/gluestack-ui.config";
 import { Loading } from "@components/Loading";
+import { SignIn } from "@screens/SignIn";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
@@ -20,13 +20,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? (
-        <Center flex={1} bg="$gray700">
-          <Text>Home</Text>
-        </Center>
-      ) : (
-        <Loading />
-      )}
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </GluestackUIProvider>
   );
 }
