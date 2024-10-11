c392bb8b72b81720add698feede20e59efa62361
Érico Maximiano Bandeira <ericobandeira@hotmail.com>
2024-10-10 23:12:40 -0300

    Add input

---
+++ src/components/Input.tsx
@@ -0,0 +1,21 @@
+import { Input as GluestackInput, InputField } from "@gluestack-ui/themed";
+import { ComponentProps } from "react";
+type Props = ComponentProps<typeof InputField>;
+export function Input({ ...props }: Props) {
+  return (
+    <GluestackInput
+      bg="$gray700"
+      h="$12"
+      px="$4"
+      borderWidth="$0"
+      borderRadius="$md"
+    >
+      <InputField
+        color="$white"
+        fontFamily="$body"
+        placeholderTextColor="$gray300"
+        {...props}
+      />
+    </GluestackInput>
+  );
+}
