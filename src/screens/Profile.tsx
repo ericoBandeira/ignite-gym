07caad6ec8ef3bf75c448976eca7b009d8d2b57a
Érico Maximiano Bandeira <ericobandeira@hotmail.com>
2024-10-17 13:55:50 -0300

    Add private routes

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -2,7 +2,7 @@ import { Center, Text } from "@gluestack-ui/themed";
 export function Profile() {
   return (
     <Center flex={1}>
-      <Text>Profile</Text>
+      <Text color="white">Profile</Text>
     </Center>
   );
 }
