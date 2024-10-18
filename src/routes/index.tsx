e749223ff0f97141449f445f831f4a93b59243f3
Érico Maximiano Bandeira <ericobandeira@hotmail.com>
2024-10-18 12:52:15 -0300

    Finish tab navigation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,20 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { Box } from "@gluestack-ui/themed";
+
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
+
+import { gluestackUIConfig } from "../../config/gluestack-ui.config";
+
 export function Routes() {
+  const theme = DefaultTheme;
+  theme.colors.background = gluestackUIConfig.tokens.colors.gray700;
+
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <Box flex={1} bg="$gray700">
+      <NavigationContainer theme={theme}>
+        <AppRoutes />
+      </NavigationContainer>
+    </Box>
   );
 }
