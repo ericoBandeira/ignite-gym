41456ec4e8b446b32a35676ce5da0bc068e1fabc
Érico Maximiano Bandeira <ericobandeira@hotmail.com>
2024-10-22 20:28:05 -0300

    Add change photo button

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,34 @@
 import { ScreenHeader } from "@components/ScreenHeader";
-import { VStack } from "@gluestack-ui/themed";
 
+import { UserPhoto } from "@components/UserPhoto";
+import { Center, Text, VStack } from "@gluestack-ui/themed";
+import { ScrollView, TouchableOpacity } from "react-native";
 export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt="$6" px="$10">
+          <UserPhoto
+            source={{ uri: "https://github.com/ericobandeira.png" }}
+            size="xl"
+            alt="Imagem do usuário"
+          />
+
+          <TouchableOpacity>
+            <Text
+              color="$green500"
+              fontFamily="$heading"
+              fontSize="$md"
+              mt="$2"
+              mb="$8"
+            >
+              Alterar Foto
+            </Text>
+          </TouchableOpacity>
+        </Center>
+      </ScrollView>
     </VStack>
   );
 }
