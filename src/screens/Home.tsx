4ca2e62dc49eefb09153985d12d575849942b2d5
Érico Maximiano Bandeira <ericobandeira@hotmail.com>
2024-10-21 21:56:39 -0300

    Add History card in History Page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -11,9 +11,6 @@ export function Home() {
     "Remada curvada",
     "Remada unilateral",
     "Levantamento terra",
-    "Levantamento terra",
-    "Levantamento terra",
-    "Levantamento terra",
   ]);
   const [groups, setGroups] = useState([
     "Costas",
@@ -33,7 +30,7 @@ export function Home() {
         renderItem={({ item }) => (
           <Group
             name={item}
-            isActive={groupSelected === item}
+            isActive={groupSelected.toLowerCase() === item.toLowerCase()}
             onPress={() => setGroupSelected(item)}
           />
         )}
