087b3852d181825b78eb9d64740179e6397f5f60
Érico Maximiano Bandeira <ericobandeira@hotmail.com>
2024-10-25 21:50:35 -0300

    Add toast

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
-import { Alert, ScrollView, TouchableOpacity } from "react-native";
-import { Center, Heading, Text, VStack } from "@gluestack-ui/themed";
-
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { ScreenHeader } from "@components/ScreenHeader";
+import { ToastMessage } from "@components/ToastMessage";
 import { UserPhoto } from "@components/UserPhoto";
-
+import { Center, Heading, Text, VStack } from "@gluestack-ui/themed";
 import * as FileSystem from "expo-file-system";
 import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+import { Alert, ScrollView, TouchableOpacity } from "react-native";
 
 export function Profile() {
   const [userPhoto, setUserPhoto] = useState(
@@ -51,6 +50,14 @@ export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
+
+      <ToastMessage
+        id="1"
+        title="Mensagem de exemplo"
+        description="asdasdakjsd asdajksdbasjdhasd"
+        action="success"
+        onClose={() => {}}
+      />
 
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt="$6" px="$10">
